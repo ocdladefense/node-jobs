6d1a1a007130167642aef728678d35ae30e6ce92
KelsieMcDonald-2003 <kelsie.lin.mcdonald@gmail.com>
2024-05-01 11:42:28 -0700

    Edited the JobPostingForm component.

--- src/js/components.js
+++ src/js/components.js
@@ -27,6 +27,7 @@ const FileUpload = function(props) {
     );
 };
 
+/*
 const JobPostingForm = function(props) {
     let job = props.job;
     let update = job.Id != null;
@@ -53,18 +54,111 @@ const JobPostingForm = function(props) {
                 <input id="datePosted" value="Enter here"/>
             )}
 
-            <label for="fileUpload">File Upload</label>
-            {/* <FileUpload></FileUpload> */}
-
             <label for="employer">Employer</label>
             {update ? (
                 <input id="employer" value={job.employer}/>
             ) : (
                 <input id="employer" value="Enter here"/>
             )}
+
+            <label for="fileUpload">File Upload</label>
+            {update ? (
+                <input id="fileUpload" type="file" value={job.FileUpload} />
+            ) : (
+                <input id="fileUpload" type="file"/>
+            )}
         </form>
     );
 };
+*/
+
+const JobPostingForm = function(props) {
+    let job = props.job;
+    let update = job.Id != null;
+    return (
+        <div class="container">
+            <form>
+                <div class="row">
+                    <div class="col">
+                        <label for="jobTitle">Job Title</label>
+                        {update ? (
+                            <input id="jobTitle" value={job.jobTitle} class="form-control"/>
+                        ) : (
+                            <input id="jobTitle" placeholder="Enter Job Title" class="form-control"/>
+                        )}
+                    </div>
+                    <div class="col">
+                        <label for="employer">Employer</label>
+                        {update ? (
+                            <input id="employer" value={job.employer} class="form-control"/>
+                        ) : (
+                            <input id="employer" placeholder="Enter the Employer" class="form-control"/>
+                        )}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label for="salary">Salary</label>
+                        {update ? (
+                            <input id="salary" value={job.salary} class="form-control"/>
+                        ) : (
+                            <input id="salary" placeholder="Enter the Salary" class="form-control"/>
+                        )}
+                    </div>
+                    <div class="col">
+                        <label for="location">Location</label>
+                        {update ? (
+                            <input id="location" value={job.location} class="form-control"/>
+                        ) : (
+                            <input id="location" placeholder="Enter the Location" class="form-control"/>
+                        )}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label for="datePosted">Posting Date</label>
+                        {update ? (
+                            <input id="datePosted" value={job.datePosted} class="form-control"/>
+                        ) : (
+                            <input id="datePosted" type="date" class="form-control"/>
+                        )}
+                    </div>
+                    <div class="col">
+                        <label for="closingDate">Closing Date</label>
+                        {update ? (
+                            <input id="closingDate" value={job.closingDate} class="form-control"/>
+                        ) : (
+                            <input id="closingDate" type="date" class="form-control"/>
+                        )}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label for="fileUpload">Upload Files</label>
+                        {update ? (
+                            <input id="fileUpload" type="file" value={job.fileUpload} class="form-control"/>
+                        ) : (
+                            <input id="fileUpload" type="file" class="form-control"/>
+                        )}
+                    </div>
+                    <div class="col">
+                        <label for="openUntilFilled">Open until filled?</label>
+                        <input id="openUntilFilled" type="checkbox" class="form-control"/>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <a href="./index.html">
+                            <input type="button" value="Cancel" class="btn btn-primary"/>
+                        </a>
+                        <input type="submit" value="Save" class="btn btn-primary"/>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
 let theData = {
     jobTitle: "Attorney",
     salary: "$1,000,000,000",
