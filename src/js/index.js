a6328a88532d0d0e87a6b018dde08c44c969a4e6
Sephgat <102762384+Sephgat@users.noreply.github.com>
2024-05-20 18:33:04 -0700

    Removed uneeded code.

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,3 @@ import Router from "./router";
 let router = new Router("#job-container");
 router.listenTo("hashchange");
 router.render();
-//let controller = new Controller("#job-container");
-//controller.listenTo("click");
-//await controller.getJobs();
-//controller.render();
