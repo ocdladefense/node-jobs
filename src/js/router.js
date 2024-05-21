5176fcf3c8f61fa7e42a7b2ebbb3aa48a05bfdd8
Sephgat <102762384+Sephgat@users.noreply.github.com>
2024-05-20 18:30:41 -0700

    Added edit handling.

--- src/js/router.js
+++ src/js/router.js
@@ -17,7 +17,7 @@ export default class Router {
         let hash = window.location.hash;
         let tree;
 
-        if (hash == "") {
+        if (hash == "" || hash == "#") {
             let c = new JobList();
             c.listenTo("click");
             await c.loadData();
@@ -29,17 +29,34 @@ export default class Router {
             await c.loadData();
             tree = c.render();
         } 
+        else if (hash.startsWith("#edit")){
+            let jobId = this.getJobId();
+            let temp = new JobList();
+            await temp.loadData();
+            let job = temp.getRecord(jobId);
+            console.log(job);
+            let c = new JobForm(job);
+            c.listenTo("click");
+            await c.loadData();
+            tree = c.render();
+        }
 
         this.view.render(tree);
     }
-
+    getJobId() {
+        let hash = window.location.hash;
+        const urlParams = new URLSearchParams(hash.substring(hash.indexOf('?')));
+        const jobId = urlParams.get('id');
+    
+        return jobId;
+    }
     listenTo(event) {
         //let elem = document.querySelector(this.selector);
         window.addEventListener(event, this);
       }
 
     handleEvent(e) {
-        console.log("hash has changed");
+        //console.log("hash has changed");
         this.render();
     }
 
