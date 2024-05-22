71e1b2c0ee92664d4222dee8cae27729408355fb
KelsieMcDonald-2003 <kelsie.lin.mcdonald@gmail.com>
2024-05-22 12:16:25 -0700

    Debugged FileUpload functionality.

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -10,10 +10,19 @@ export default class FileUpload {
     this.fileUrl = url;
   }
 
+  // getFileInput(id) {
+  //   let elem = document.getElementById(id);
+  //   return elem.files[0];
+  // }
+
   getFileInput(id) {
     let elem = document.getElementById(id);
+    // Add a change event listener to the file input element
+    elem.addEventListener('change', (event) => {
+      console.log(event.target.files[0]); // Log the selected file details to the console
+    });
     return elem.files[0];
-  }
+  }  
 
   async handleEvent(e) {
     let target = e.target;
@@ -54,24 +63,42 @@ export default class FileUpload {
     // await this.api.uploadFile("Job__c", job.Id, file);
   }
 
+  // render() {
+  //   let multipleFilesEnabled = false;
+
+  //   return (
+  //     <div class="mb-3">
+  //       <label for="file-upload" class="form-label">Upload Files</label>
+  //       <div class="input-group">
+  //         {this.fileUrl != "" ? (
+  //           <span>{this.fileUrl}</span>
+  //         ) : (
+  //           <input type="file" class="form-control-file" id="file-upload" value={this.fileUrl} aria-describedby="file-upload-help" />
+  //         )}
+  //         <input type="button" data-action="upload" value="Upload File" onClick={this.handleEvent.bind(this)} />
+  //         <input type="button" data-action="remove" value="Remove File" onClick={this.handleEvent.bind(this)} />
+  //         {multipleFilesEnabled ? (<input type="button" data-action="add" value="Add File" onClick={this.handleEvent.bind(this)} />) : ("")} 
+  //       </div>
+  //       <div id="file-upload-help" class="form-text fs-6">Any files relevant to the position (insert data constraints here).</div>
+  //     </div>    
+  //   );
+  // }
+
   render() {
     let multipleFilesEnabled = false;
-
+  
     return (
       <div class="mb-3">
         <label for="file-upload" class="form-label">Upload Files</label>
         <div class="input-group">
-          {this.fileUrl != "" ? (
-            <span>{this.fileUrl}</span>
-          ) : (
-            <input type="file" class="form-control-file" id="file-upload" value={this.fileUrl} aria-describedby="file-upload-help" />
-          )}
-          <input type="button" data-action="upload" value="Upload File" onClick={this.handleEvent.bind(this)} />
+          {this.fileUrl != "" ? (<span>{this.fileUrl}</span>) : null}
+          <input type="file" class="form-control-file" id="file-upload" value={this.fileUrl} aria-describedby="file-upload-help" />
+          <input type="button" data-action="upload" value="Upload File" onClick={() => document.getElementById('file-upload').click()} />
           <input type="button" data-action="remove" value="Remove File" onClick={this.handleEvent.bind(this)} />
           {multipleFilesEnabled ? (<input type="button" data-action="add" value="Add File" onClick={this.handleEvent.bind(this)} />) : ("")} 
         </div>
         <div id="file-upload-help" class="form-text fs-6">Any files relevant to the position (insert data constraints here).</div>
       </div>    
     );
-  }
+  }  
 }
