d37cdc7af5b20ad8fa32146906930928e8c3c6aa
Sephgat <102762384+Sephgat@users.noreply.github.com>
2024-06-04 09:16:02 -0700

    Added commits and a way to not use mock data.

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,19 @@ import "../css/form-design.css";
 import Router from "./Router.js";
 import HttpClient from "@ocdla/lib-http/HttpClient.js";
 import SalesforceRecordsMock from "./SalesforceRecordsMock.js";
+// add inline comment's
 
+let usemock = USE_MOCK_RECORDS;
+// Checks to see if we want to use the mock records.
+if(usemock)
+{
 HttpClient.register(INSTANCE_URL, new SalesforceRecordsMock());
+}
+// Initialize router to handle URL requests.
 let router = new Router("#job-container");
+
+// Listen for changes in the URL hash (the part of the URL after the # symbol).
 router.listenTo("hashchange");
+
+// Renders the intial veiw based on the current URL hash.
 router.render();
