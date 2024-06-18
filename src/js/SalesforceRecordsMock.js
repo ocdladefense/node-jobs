b05237cd24fa6b284cb2a5ef84448769fb57d3c1
Charlese Muchmore <charlymuchmore@gmail.com>
2024-06-18 16:37:58 -0700

    Made mock records a seperate json file that is used by SalesforceRecordsMock.js.

--- src/js/SalesforceRecordsMock.js
+++ src/js/SalesforceRecordsMock.js
@@ -12,12 +12,13 @@ Fusce et lectus et mauris ultricies ullamcorper non id purus. Curabitur dapibus 
 export default class SalesforceJobMock extends HttpMock {
   #list;
 
-  constructor() {
+  constructor(jsonData) {
     super();
+    this.records = jsonData;
 
     let tmp = [];
 
-    SalesforceJobMock.records.records.forEach((record) => {
+    this.records.records.forEach((record) => {
       tmp.push([record.Id, record]);
     });
 
@@ -71,49 +72,6 @@ export default class SalesforceJobMock extends HttpMock {
     return this.list.set(recordId, job);
   }
 
-  static records = {
-    records: [
-      {
-        OwnerId: "0",
-        Id: "0",
-        Name: "Legal Maverick",
-        Salary__c: "$80,000",
-        PostingDate__c: "2024-04-20",
-        ClosingDate__c: "2024-05-29",
-        FileUrl__c: "https://my-domain.com/document1",
-        Employer__c: "Veritas Law Group",
-        Location__c: "Rivertown Junction",
-        OpenUntilFilled__c: false,
-        Description__c: exampleDescription,
-      },
-      {
-        OwnerId: "1",
-        Id: "1",
-        Name: "Trial Whisperer",
-        Salary__c: "$110,000",
-        PostingDate__c: "2024-02-28",
-        ClosingDate__c: "2024-05-10",
-        FileUrl__c: "https:/this-domain.org/documents/requirements",
-        Employer__c: "JusticeShield Attorneys",
-        Location__c: "Cedarwood Heights",
-        OpenUntilFilled__c: true,
-        Description__c: exampleDescription,
-      },
-      {
-        OwnerId: "2",
-        Id: "2",
-        Name: "Justice Architect",
-        Salary__c: "$96,000",
-        PostingDate__c: "2024-04-17",
-        ClosingDate__c: "2024-06-01",
-        FileUrl__c: "https://a-domain.law/justice-architect",
-        Employer__c: "Liberty Legal Associates",
-        Location__c: "Haborview Bay",
-        OpenUntilFilled__c: false,
-        Description__c: exampleDescription,
-      },
-    ],
-  };
 }
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,12 @@ import Router from "./Router.js";
 import HttpClient from "@ocdla/lib-http/HttpClient.js";
 import SalesforceRecordsMock from "./SalesforceRecordsMock.js";
 import colloquial from "@ocdla/lib-date/DateConvert.js";
+import jsonData from '../test-data/jobs.json';
+
+if(USE_MOCK_RECORDS) {
+    HttpClient.register(INSTANCE_URL, new SalesforceRecordsMock(jsonData));
+}
+
 
 // Colloquial function testing...
 // If today's date is May 31, 2024 then:
@@ -15,11 +21,6 @@ console.log(colloquial("2024-05-31")); // Returns "today".
 console.log(colloquial("2024-06-01")); //returns "tomorrow"
 console.log(colloquial("2024-06-02")); //returns "in 2 days";
 console.log(colloquial("2024-06-03")); //returns "in 3 days";
-
-
-if(USE_MOCK_RECORDS) {
-    HttpClient.register(INSTANCE_URL, new SalesforceRecordsMock());
-}
 
 let router = new Router("#job-container");
 router.listenTo("hashchange");
